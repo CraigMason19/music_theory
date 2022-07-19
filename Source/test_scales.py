7a0ccb93cb2f9668bde6fe5453b28ad03f596a17
SadHappy <craigjm19@gmail.com>
2022-07-19 08:52:56 +0100

    Added tests for current formulas using A, C & F

--- Source/test_scales.py
+++ Source/test_scales.py
@@ -20,75 +20,79 @@ class TestScales(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('setupClass')
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        print('teardownClass')
+        pass
 
     #---------------------------------------------------------------------------
     # setUp and tearDown run before every single test.
     #---------------------------------------------------------------------------
     def setUp(self):
-        # print('\tsetUp')
         pass
 
     def tearDown(self):
-        # print('\ttearDown')
         pass
 
     #---------------------------------------------------------------------------
-    # Tests
+    # Tests - (Only tests a, c, and f)
     #---------------------------------------------------------------------------
 
     #region MajorScales
     def test_a_major_scale(self):
-        print('\t\ttest_a_major')
-
         scale = s.Scale(Note.A, s.ScaleType.Major)
         self.assertEqual(scale.notes, [Note.A, Note.B, Note.Db, Note.D, Note.E, Note.Gb, Note.Ab])
 
     def test_c_major_scale(self):
-        print('\t\ttest_c_major')
-
         scale = s.Scale(Note.C, s.ScaleType.Major)
         self.assertEqual(scale.notes, [Note.C, Note.D, Note.E, Note.F, Note.G, Note.A, Note.B])
+
+    def test_f_major_scale(self):
+        scale = s.Scale(Note.F, s.ScaleType.Major)
+        self.assertEqual(scale.notes, [Note.F, Note.G, Note.A, Note.Bb, Note.C, Note.D, Note.E])
     #endregion
 
     #region MinorScales
-    def test_c_minor_scale(self):
-        print('\t\ttest_c_minor')
+    def test_a_minor_scale(self):
+        scale = s.Scale(Note.A, s.ScaleType.Minor)
+        self.assertEqual(scale.notes, [Note.A, Note.B, Note.C, Note.D, Note.E, Note.F, Note.G])
 
+    def test_c_minor_scale(self):
         scale = s.Scale(Note.C, s.ScaleType.Minor)
         self.assertEqual(scale.notes, [Note.C, Note.D, Note.Eb, Note.F, Note.G, Note.Ab, Note.Bb])
+
+    def test_f_minor_scale(self):
+        scale = s.Scale(Note.F, s.ScaleType.Minor)
+        self.assertEqual(scale.notes, [Note.F, Note.G, Note.Ab, Note.Bb, Note.C, Note.Db, Note.Eb])
     #endregion
 
     #region MajorPentatonicScales
     def test_a_major_pentatonic(self):
-        print('\t\ttest_a_major_pentatonic')
-
         scale = s.Scale(Note.A, s.ScaleType.MajorPentatonic)
         self.assertEqual(scale.notes, [Note.A, Note.B, Note.Db, Note.E, Note.Gb])
     
     def test_c_major_pentatonic(self):
-        print('\t\ttest_c_major_pentatonic')
-
         scale = s.Scale(Note.C, s.ScaleType.MajorPentatonic)
         self.assertEqual(scale.notes, [Note.C, Note.D, Note.E, Note.G, Note.A])
+
+    def test_f_major_pentatonic(self):
+        scale = s.Scale(Note.F, s.ScaleType.MajorPentatonic)
+        self.assertEqual(scale.notes, [Note.F, Note.G, Note.A, Note.C, Note.D])
     #endregion
 
     #region MinorPentatonicScales
     def test_a_minor_pentatonic(self):
-        print('\t\ttest_a_minor_pentatonic')
-
         scale = s.Scale(Note.A, s.ScaleType.MinorPentatonic)
         self.assertEqual(scale.notes, [Note.A, Note.C, Note.D, Note.E, Note.G])
 
     def test_c_minor_pentatonic(self):
-        print('\t\ttest_c_minor_pentatonic')
-
         scale = s.Scale(Note.C, s.ScaleType.MinorPentatonic)
         self.assertEqual(scale.notes, [Note.C, Note.Eb, Note.F, Note.G, Note.Bb])
+
+    def test_f_minor_pentatonic(self):
+        scale = s.Scale(Note.F, s.ScaleType.MinorPentatonic)
+        self.assertEqual(scale.notes, [Note.F, Note.Ab, Note.Bb, Note.C, Note.Eb])
     #endregion
 
 if __name__ == '__main__':
