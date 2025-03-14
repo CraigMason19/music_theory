804b2c6d4790f6bd095cb9047418b545660b505c
SadHappy <craigjm19@gmail.com>
2025-03-14 21:08:04 +0000

    Added instrument_creator.py and appropriate tests

---
+++ music_theory/instrument_creator.py
@@ -0,0 +1,47 @@
+from music_theory.instrument import StringInstrument
+from music_theory.notes import Note, Interval, transpose
+
+STANDARD_BASS_TUNING_INTERVALS = [
+    Interval.Unison, Interval.P4, Interval.P4, Interval.P4
+] 
+
+STANDARD_GUITAR_TUNING_INTERVALS = [
+    Interval.Unison, Interval.P4, Interval.P4, Interval.P4, Interval.M3, Interval.P4
+] 
+
+def create_standard_bass(note=Note.E):
+    return StringInstrument.from_tuning_intervals(note, STANDARD_BASS_TUNING_INTERVALS)
+
+def create_drop_bass(note=Note.D):
+    bass = StringInstrument.from_tuning_intervals(transpose(note, Interval.M2, "up"), STANDARD_BASS_TUNING_INTERVALS)
+    bass.tuning[0] = note
+    return bass
+
+def create_standard_guitar(note=Note.E):
+    return StringInstrument.from_tuning_intervals(note, STANDARD_GUITAR_TUNING_INTERVALS)
+
+def create_drop_guitar(note=Note.D):
+    guitar = StringInstrument.from_tuning_intervals(transpose(note, Interval.M2, "up"), STANDARD_GUITAR_TUNING_INTERVALS)
+    guitar.tuning[0] = note
+    return guitar
+    
+def create_ukulele():
+        return StringInstrument([Note.G, Note.C, Note.E, Note.A])
+
+# Standard tunings
+E_STANDARD_GUITAR = create_standard_guitar()
+HALF_STEP_DOWN_GUITAR = create_standard_guitar(Note.Eb)
+D_STANDARD_GUITAR = create_standard_guitar(Note.D)
+C_STANDARD_GUITAR = create_standard_guitar(Note.C)
+
+# Drop tunings
+DROP_D_GUITAR = create_drop_guitar(Note.D)
+DROP_C_GUITAR = create_drop_guitar(Note.C)
+
+# Basses
+E_STANDARD_BASS = create_standard_bass()
+DROP_D_BASS = create_drop_bass(Note.D)
+
+# Extended-range instruments
+FIVE_STRING_BASS = StringInstrument([Note.B] + E_STANDARD_BASS.tuning)
+SEVEN_STRING_GUITAR = StringInstrument([Note.B] + E_STANDARD_GUITAR.tuning)
---
+++ music_theory_tests/test_instrument_creator.py
@@ -0,0 +1,124 @@
+#-------------------------------------------------------------------------------
+# Name:        test_instrument_creator.py
+#
+# Notes:       A test for various instrument setups
+#
+# Links:        
+#
+# TODO:        
+#-------------------------------------------------------------------------------
+
+import unittest
+
+import _setup
+
+from music_theory.notes import Note
+from music_theory.instrument_creator import create_standard_bass, create_drop_bass, create_standard_guitar, create_drop_guitar, create_ukulele
+
+class TestChords(unittest.TestCase):
+    #---------------------------------------------------------------------------
+    # setUpClass and tearDownClass run before and after all tests, called once
+    # NOTE - the camelCase syntax. Important that they are named this way.
+    #---------------------------------------------------------------------------
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    #---------------------------------------------------------------------------
+    # setUp and tearDown run before every single test.
+    #---------------------------------------------------------------------------
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    #---------------------------------------------------------------------------
+    # Tests
+    #---------------------------------------------------------------------------
+
+    #region Bass
+
+    def test_default_standard_bass_00(self):
+        bass = create_standard_bass()
+        expected = [Note.E, Note.A, Note.D, Note.G]
+        self.assertEqual(bass.tuning, expected)
+    
+    def test_default_standard_bass_01(self):
+        bass = create_standard_bass(Note.E)
+        expected = [Note.E, Note.A, Note.D, Note.G]
+        self.assertEqual(bass.tuning, expected)
+
+    def test_standard_bass_02(self):
+        bass = create_standard_bass(Note.D)
+        expected = [Note.D, Note.G, Note.C, Note.F]
+        self.assertEqual(bass.tuning, expected)
+
+    def test_default_drop_bass_00(self):
+        bass = create_drop_bass()
+        expected = [Note.D, Note.A, Note.D, Note.G]
+        self.assertEqual(bass.tuning, expected)
+    
+    def test_default_drop_bass_01(self):
+        bass = create_drop_bass(Note.D)
+        expected = [Note.D, Note.A, Note.D, Note.G]
+        self.assertEqual(bass.tuning, expected)
+
+    def test_drop_bass_02(self):
+        bass = create_drop_bass(Note.C)
+        expected = [Note.C, Note.G, Note.C, Note.F]
+        self.assertEqual(bass.tuning, expected)
+
+    #endregion
+
+    #region Guitar
+
+    def test_default_standard_guitar_00(self):
+        guitar = create_standard_guitar()
+        expected = [Note.E, Note.A, Note.D, Note.G, Note.B, Note.E]
+        self.assertEqual(guitar.tuning, expected)
+    
+    def test_default_standard_guitar_01(self):
+        guitar = create_standard_guitar(Note.E)
+        expected = [Note.E, Note.A, Note.D, Note.G, Note.B, Note.E]
+        self.assertEqual(guitar.tuning, expected)
+
+    def test_standard_guitar_02(self):
+        guitar = create_standard_guitar(Note.E)
+        expected = [Note.E, Note.A, Note.D, Note.G, Note.B, Note.E]
+        self.assertEqual(guitar.tuning, expected)
+
+    def test_default_drop_guitar_00(self):
+        guitar = create_drop_guitar()
+        expected = [Note.D, Note.A, Note.D, Note.G, Note.B, Note.E]
+        self.assertEqual(guitar.tuning, expected)
+    
+    def test_default_guitar_bass_01(self):
+        guitar = create_drop_guitar(Note.D)
+        expected = [Note.D, Note.A, Note.D, Note.G, Note.B, Note.E]
+        self.assertEqual(guitar.tuning, expected)
+
+    def test_drop_guitar_02(self):
+        guitar = create_drop_guitar(Note.C)
+        expected = [Note.C, Note.G, Note.C, Note.F, Note.A, Note.D]
+        self.assertEqual(guitar.tuning, expected)
+
+    #endregion
+
+    #region Other Instruments
+
+    def test_default_ukelele(self):
+        ukelele = create_ukulele()
+        expected = [Note.G, Note.C, Note.E, Note.A]
+        self.assertEqual(ukelele.tuning, expected)
+
+    #endregion
+
+if __name__ == '__main__':
+    unittest.main()
