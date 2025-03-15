8b99129a3914759cc021dba38e6d011da1f73afa
SadHappy <craigjm19@gmail.com>
2025-03-15 09:36:05 +0000

    Added a note_in_chord method. Create the test suite

--- music_theory/instrument.py
+++ music_theory/instrument.py
@@ -1,22 +1,11 @@
-import copy
+import re
 
 from enum import Enum
 
 from music_theory.notes import Note, Interval, transpose, notes_to_string
 
-STANDARD_BASS_TUNING_INTERVALS = [
-    Interval.Unison, Interval.P4, Interval.P4, Interval.P4
-] 
-
-STANDARD_GUITAR_TUNING_INTERVALS = [
-    Interval.Unison, Interval.P4, Interval.P4, Interval.P4, Interval.M3, Interval.P4
-] 
-
-class String(Enum):
-    (first, second, third, fourth, fifth, sixth) = range(6)
-
-class TuningType:
-    (standard, drop, open) = range(3)
+def extract_frets(line):
+    return [int(fret) for fret in re.findall(r"\d+", line)]
 
 class StringInstrument:
     def __init__(self, tuning):
@@ -35,11 +24,11 @@ class StringInstrument:
 
         return cls(tuning)   
         
-    def with_capo(self, fret):
-        new_instrument = copy.copy(self)
-        new_instrument.tuning = [transpose(n, Interval.from_index(fret), "u") for n in self.tuning]
-
-        return new_instrument
+    def add_capo(self, fret):
+        if fret <= 0:
+            raise ValueError("Capo must be placed on a positive fret")
+        
+        self.tuning = [transpose(n, Interval.from_index(fret), "u") for n in self.tuning]
 
     def adjust_string(self, string_index, interval, direction="u"):
         try:            
@@ -51,64 +40,31 @@ class StringInstrument:
         self.tuning = [transpose(string, interval, direction="d") for string in self.tuning]
     
     def note_at_fret(self, string_index, fret):
-        if fret < -1:
-            raise ValueError(f"Frets can't be negative")
+        if fret < 0:
+            raise ValueError("Frets can't be negative")
         
         try:            
             return Note.from_index(self.tuning[string_index].value + fret)
         except:
-            raise ValueError(f"Incorrect string index {string_index}")     
+            raise ValueError(f"Incorrect string index {string_index}")   
 
-    def notes_at_frets(self, frets):
-        if self.num_strings != len(frets):
-            raise ValueError(f"All strings must be accounted for")
-        
-        #hmm
-        
-        # frets = [fret for fret in frets if fret not in {"x", "X", "-"}]
+    def notes_in_chord(self, chord_str):
+        lines =  chord_str.split()
 
-        # return [Note.from_index(self.tuning[string_index].value + fret)]
+        if len(lines) != self.num_strings:
+            raise ValueError(f"Expected {self.num_strings} strings, but got {len(lines)}")
+
+        notes = []
+        frets = extract_frets(chord_str)
+
+        for string_index, string in enumerate(lines):
+            frets = extract_frets(string)
+            notes.extend([self.note_at_fret(string_index, fret) for fret in frets])
+
+        return notes  
 
     def __str__(self):
         return f"{notes_to_string(self.tuning)}"
 
     def __repr__(self):
         return f"StringInstrument({self.num_strings}, [{notes_to_string(self.tuning)}])"
-
-
-def create_bass(note=Note.E, tuning_type=TuningType.standard):
- 
-    match tuning_type:
-        case TuningType.drop:
-            guitar = StringInstrument.from_tuning_intervals(transpose(note, Interval.M2, "up"), STANDARD_BASS_TUNING_INTERVALS)
-            guitar.adjust_string(0, Interval.M2, "down")
-            return guitar
-
-        case _:
-            return StringInstrument.from_tuning_intervals(note, STANDARD_BASS_TUNING_INTERVALS)
-
-def create_guitar(note=Note.E, tuning_type=TuningType.standard):
- 
-    match tuning_type:
-        case TuningType.drop:
-            guitar = StringInstrument.from_tuning_intervals(transpose(note, Interval.M2, "up"), STANDARD_GUITAR_TUNING_INTERVALS)
-            guitar.adjust_string(0, Interval.M2, "down")
-            return guitar
-
-        case _:
-            return StringInstrument.from_tuning_intervals(note, STANDARD_GUITAR_TUNING_INTERVALS)
-
-def created_ukulele():
-    return StringInstrument([Note.G, Note.C, Note.E, Note.A])
-
-E_STANDARD_GUITAR = create_guitar()
-HALF_STEP_DOWN_GUITAR = create_guitar(Note.Eb)
-D_STANDARD_GUITAR = create_guitar(Note.D)
-C_STANDARD_GUITAR = create_guitar(Note.C)
-
-DROP_D_GUITAR = create_guitar(Note.D, TuningType.drop)
-DROP_C_GUITAR = create_guitar(Note.C, TuningType.drop)
-   
-FIVE_STRING_BASS = StringInstrument([Note.B] + create_guitar().tuning)
-
-UKELELE = created_ukulele()
---
+++ music_theory_tests/test_instrument.py
@@ -0,0 +1,96 @@
+#-------------------------------------------------------------------------------
+# Name:        test_instrument.py
+#
+# Notes:       A test suite for the instrument script.
+#
+# Links:        
+#
+# TODO:        
+#-------------------------------------------------------------------------------
+
+import unittest
+
+import _setup
+
+from music_theory.notes import Note, Interval
+from music_theory.instrument import StringInstrument
+from music_theory.instrument_creator import create_standard_guitar, E_STANDARD_GUITAR
+
+class TestChords(unittest.TestCase):
+    #---------------------------------------------------------------------------
+    # setUpClass and tearDownClass run before and after all tests, called once
+    # NOTE - the camelCase syntax. Important that they are named this way.
+    #---------------------------------------------------------------------------
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    #---------------------------------------------------------------------------
+    # setUp and tearDown run before every single test.
+    #---------------------------------------------------------------------------
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    #---------------------------------------------------------------------------
+    # Tests
+    #---------------------------------------------------------------------------
+
+    #region Creation
+
+    def test_custom_instrument_00(self):
+        instrument = StringInstrument([Note.B, Note.E, Note.A, Note.D, Note.G])
+        expected = [Note.B, Note.E, Note.A, Note.D, Note.G]
+        self.assertEqual(instrument.tuning, expected)
+
+    def test_custom_instrument_01(self):
+        instrument = StringInstrument([Note.B, Note.E, Note.A, Note.D, Note.G])
+        expected = 5
+        self.assertEqual(instrument.num_strings, expected)
+
+    def test_custom_instrument_no_strings(self):
+        self.assertRaises(ValueError, StringInstrument, [])
+
+    def test_tuning_intervals_00(self):
+        intervals = [Interval.Unison, Interval.P4, Interval.P4, Interval.P4, Interval.M3, Interval.P4]
+        instrument = StringInstrument.from_tuning_intervals(Note.E, intervals)
+        
+        self.assertEqual(instrument.tuning, E_STANDARD_GUITAR.tuning)
+
+    #endregion
+
+    #region Capo
+    def test_add_a_capo_at_second_fret(self):
+        guitar = create_standard_guitar(Note.E)
+        guitar.add_capo(2)
+
+        expected_tuning = [Note.Gb, Note.B, Note.E, Note.A, Note.Db, Note.Gb]
+        self.assertEqual(guitar.tuning, expected_tuning)
+
+    def test_add_a_capo_at_fourth_fret(self):
+        guitar = create_standard_guitar(Note.E)
+        guitar.add_capo(4)
+
+        expected_tuning = [Note.Ab, Note.Db, Note.Gb, Note.B, Note.Eb, Note.Ab]
+        self.assertEqual(guitar.tuning, expected_tuning)
+
+    def test_add_a_capo_at_zero_fret(self):
+        guitar = create_standard_guitar(Note.E)
+        self.assertRaises(ValueError, guitar.add_capo, 0)
+
+    def test_add_a_capo_at_negative_fret(self):
+        guitar = create_standard_guitar(Note.E)
+        self.assertRaises(ValueError, guitar.add_capo, -1)
+
+    #endregion
+
+if __name__ == '__main__':
+    unittest.main()
