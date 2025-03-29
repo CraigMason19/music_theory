4436952ee48511e546bdf9d4ad41a45a8063962d
SadHappy <craigjm19@gmail.com>
2025-03-29 13:52:12 +0000

    complete coverage for utils

--- music_theory_tests/test_utils.py
+++ music_theory_tests/test_utils.py
@@ -51,5 +51,27 @@ class TestNotes(unittest.TestCase):
         expected = 0
         self.assertEqual(result, expected)
 
+    def test_index_to_range_positive_00(self):
+        result = index_to_range(144)
+        expected = 0
+        self.assertEqual(result, expected)
+
+    def test_index_to_range_positive_01(self):
+        result = index_to_range(121)
+        expected = 1
+        self.assertEqual(result, expected)
+
+    def test_direction_in_up_directions_correct(self):
+        self.assertIn('up', UP_DIRECTIONS)
+
+    def test_direction_in_up_directions_incorrect(self):
+        self.assertNotIn('uppy', UP_DIRECTIONS)
+
+    def test_direction_in_down_directions_correct(self):
+        self.assertIn('down', DOWN_DIRECTIONS)
+
+    def test_direction_in_down_directions_incorrect(self):
+        self.assertNotIn('downy', DOWN_DIRECTIONS)
+
 if __name__ == '__main__': # pragma: no cover
     unittest.main()
