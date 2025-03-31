bfba4ddf2757fb79f65196752243ba6338cdfba1
SadHappy <craigjm19@gmail.com>
2025-03-31 10:43:19 +0100

    Changes Scale test imports

--- music_theory_tests/test_scales.py
+++ music_theory_tests/test_scales.py
@@ -14,8 +14,7 @@ import _setup
 
 from music_theory.notes import Note
 from music_theory.intervals import Interval
-
-import music_theory.scales as s
+from music_theory.scales import ScaleType, Scale, _intervals_from_numerics, _intervals_from_steps, _notes_from_intervals, _notes_from_steps
 
 class TestScales(unittest.TestCase):
     #---------------------------------------------------------------------------
@@ -48,127 +47,124 @@ class TestScales(unittest.TestCase):
     #region ScaleType
 
     def test_scale_type_items(self):
-        expected = [s.ScaleType.Major, s.ScaleType.Minor, s.ScaleType.MajorPentatonic, s.ScaleType.MinorPentatonic, s.ScaleType.Ionian,
-            s.ScaleType.Dorian, s.ScaleType.Phrygian, s.ScaleType.Lydian, s.ScaleType.Mixolydian, s.ScaleType.Aeolian, s.ScaleType.Locrian,
-            s.ScaleType.Blues, s.ScaleType.HarmonicMinor, s.ScaleType.MelodicMinor]
-        self.assertEqual(s.ScaleType.items(), expected)
+        expected = [ScaleType.Major, ScaleType.Minor, ScaleType.MajorPentatonic, ScaleType.MinorPentatonic, ScaleType.Ionian,
+            ScaleType.Dorian, ScaleType.Phrygian, ScaleType.Lydian, ScaleType.Mixolydian, ScaleType.Aeolian, ScaleType.Locrian,
+            ScaleType.Blues, ScaleType.HarmonicMinor, ScaleType.MelodicMinor]
+        self.assertEqual(ScaleType.items(), expected)
 
     def test_scale_type_all_alias(self):
-        expected = [s.ScaleType.Major, s.ScaleType.Minor, s.ScaleType.MajorPentatonic, s.ScaleType.MinorPentatonic, s.ScaleType.Ionian,
-            s.ScaleType.Dorian, s.ScaleType.Phrygian, s.ScaleType.Lydian, s.ScaleType.Mixolydian, s.ScaleType.Aeolian, s.ScaleType.Locrian,
-            s.ScaleType.Blues, s.ScaleType.HarmonicMinor, s.ScaleType.MelodicMinor]
-        self.assertEqual(s.ScaleType.all(), expected)
+        expected = [ScaleType.Major, ScaleType.Minor, ScaleType.MajorPentatonic, ScaleType.MinorPentatonic, ScaleType.Ionian,
+            ScaleType.Dorian, ScaleType.Phrygian, ScaleType.Lydian, ScaleType.Mixolydian, ScaleType.Aeolian, ScaleType.Locrian,
+            ScaleType.Blues, ScaleType.HarmonicMinor, ScaleType.MelodicMinor]
+        self.assertEqual(ScaleType.all(), expected)
 
     def test_scale_type_items_same_as_all_alias(self):
-        self.assertListEqual(s.ScaleType.items(), s.ScaleType.all())
+        self.assertListEqual(ScaleType.items(), ScaleType.all())
 
     def test_random_scale_type_validity(self):
-        type = s.ScaleType.random()
-        self.assertIn(type, list(s.ScaleType))
+        type = ScaleType.random()
+        self.assertIn(type, list(ScaleType))
 
     def test_scale_type_str(self):
-        type = s.ScaleType.HarmonicMinor
+        type = ScaleType.HarmonicMinor
         self.assertEqual(str(type), "HarmonicMinor")
 
     def test_scale_type_repr(self):
-        type = s.ScaleType.HarmonicMinor
+        type = ScaleType.HarmonicMinor
         self.assertEqual(repr(type), "ScaleType.HarmonicMinor")
 
     #endregion
 
-
-
-
     #region MajorScales
     def test_a_major_scale(self):
-        scale = s.Scale(Note.A, s.ScaleType.Major)
+        scale = Scale(Note.A, ScaleType.Major)
         self.assertEqual(scale.notes, [Note.A, Note.B, Note.Db, Note.D, Note.E, Note.Gb, Note.Ab])
 
     def test_c_major_scale(self):
-        scale = s.Scale(Note.C, s.ScaleType.Major)
+        scale = Scale(Note.C, ScaleType.Major)
         self.assertEqual(scale.notes, [Note.C, Note.D, Note.E, Note.F, Note.G, Note.A, Note.B])
 
     def test_f_major_scale(self):
-        scale = s.Scale(Note.F, s.ScaleType.Major)
+        scale = Scale(Note.F, ScaleType.Major)
         self.assertEqual(scale.notes, [Note.F, Note.G, Note.A, Note.Bb, Note.C, Note.D, Note.E])
     #endregion
 
     #region MinorScales
     def test_a_minor_scale(self):
-        scale = s.Scale(Note.A, s.ScaleType.Minor)
+        scale = Scale(Note.A, ScaleType.Minor)
         self.assertEqual(scale.notes, [Note.A, Note.B, Note.C, Note.D, Note.E, Note.F, Note.G])
 
     def test_c_minor_scale(self):
-        scale = s.Scale(Note.C, s.ScaleType.Minor)
+        scale = Scale(Note.C, ScaleType.Minor)
         self.assertEqual(scale.notes, [Note.C, Note.D, Note.Eb, Note.F, Note.G, Note.Ab, Note.Bb])
 
     def test_f_minor_scale(self):
-        scale = s.Scale(Note.F, s.ScaleType.Minor)
+        scale = Scale(Note.F, ScaleType.Minor)
         self.assertEqual(scale.notes, [Note.F, Note.G, Note.Ab, Note.Bb, Note.C, Note.Db, Note.Eb])
     #endregion
 
     #region MajorPentatonicScales
     def test_a_major_pentatonic(self):
-        scale = s.Scale(Note.A, s.ScaleType.MajorPentatonic)
+        scale = Scale(Note.A, ScaleType.MajorPentatonic)
         self.assertEqual(scale.notes, [Note.A, Note.B, Note.Db, Note.E, Note.Gb])
     
     def test_c_major_pentatonic(self):
-        scale = s.Scale(Note.C, s.ScaleType.MajorPentatonic)
+        scale = Scale(Note.C, ScaleType.MajorPentatonic)
         self.assertEqual(scale.notes, [Note.C, Note.D, Note.E, Note.G, Note.A])
 
     def test_f_major_pentatonic(self):
-        scale = s.Scale(Note.F, s.ScaleType.MajorPentatonic)
+        scale = Scale(Note.F, ScaleType.MajorPentatonic)
         self.assertEqual(scale.notes, [Note.F, Note.G, Note.A, Note.C, Note.D])
     #endregion
 
     #region MinorPentatonicScales
     def test_a_minor_pentatonic(self):
-        scale = s.Scale(Note.A, s.ScaleType.MinorPentatonic)
+        scale = Scale(Note.A, ScaleType.MinorPentatonic)
         self.assertEqual(scale.notes, [Note.A, Note.C, Note.D, Note.E, Note.G])
 
     def test_c_minor_pentatonic(self):
-        scale = s.Scale(Note.C, s.ScaleType.MinorPentatonic)
+        scale = Scale(Note.C, ScaleType.MinorPentatonic)
         self.assertEqual(scale.notes, [Note.C, Note.Eb, Note.F, Note.G, Note.Bb])
 
     def test_f_minor_pentatonic(self):
-        scale = s.Scale(Note.F, s.ScaleType.MinorPentatonic)
+        scale = Scale(Note.F, ScaleType.MinorPentatonic)
         self.assertEqual(scale.notes, [Note.F, Note.Ab, Note.Bb, Note.C, Note.Eb])
     #endregion
 
     #region NotesFromSteps
     def test_c_major_notes_from_steps(self):
-        notes = s._notes_from_steps(Note.C, 'wwhwwwh')
+        notes = _notes_from_steps(Note.C, 'wwhwwwh')
         expected = [Note.C, Note.D, Note.E, Note.F, Note.G, Note.A, Note.B]
         self.assertEqual(notes, expected)
 
     def test_Bb_mixolydian_notes_from_steps(self):
-        notes = s._notes_from_steps(Note.Bb, ['w', 'w', 'h', 'w', 'w', 'h', 'w'])
+        notes = _notes_from_steps(Note.Bb, ['w', 'w', 'h', 'w', 'w', 'h', 'w'])
         expected = [Note.Bb, Note.C, Note.D, Note.Eb, Note.F, Note.G, Note.Ab]
         self.assertEqual(notes, expected)
     #endregion
 
     #region IntervalsFromSteps
     def test_major_intervals_from_steps(self):
-        intervals = s._intervals_from_steps(['w', 'w', 'h', 'w', 'w', 'w', 'h'])
+        intervals = _intervals_from_steps(['w', 'w', 'h', 'w', 'w', 'w', 'h'])
         expected = [Interval.Unison, Interval.M2, Interval.M3, Interval.P4, Interval.P5, Interval.M6, Interval.M7]
         self.assertEqual(intervals, expected)
     #endregion
 
     #region IntervalsFromNumerics
     def test_major_intervals_from_numerics(self):
-        intervals = s._intervals_from_numerics(['1', '2', '3', '4', '5', '6', '7'])
+        intervals = _intervals_from_numerics(['1', '2', '3', '4', '5', '6', '7'])
         expected = [Interval.Unison, Interval.M2, Interval.M3, Interval.P4, Interval.P5, Interval.M6, Interval.M7]
         self.assertEqual(intervals, expected)
 
     def test_harmonic_minor_intervals_from_numerics(self):
-        intervals = s._intervals_from_numerics(['1', '2', 'b3', '4', '5', 'b6', '7'])
+        intervals = _intervals_from_numerics(['1', '2', 'b3', '4', '5', 'b6', '7'])
         expected = [Interval.Unison, Interval.M2, Interval.m3, Interval.P4, Interval.P5, Interval.m6, Interval.M7]
         self.assertEqual(intervals, expected)
     #endregion
 
     #region NotesFromIntervals
     def test_C_major_notes_from_intervals(self):
-        notes = s._notes_from_intervals(Note.C, [Interval.Unison, Interval.M2, Interval.M3, Interval.P4, Interval.P5, Interval.M6, Interval.M7])
+        notes = _notes_from_intervals(Note.C, [Interval.Unison, Interval.M2, Interval.M3, Interval.P4, Interval.P5, Interval.M6, Interval.M7])
         expected = [Note.C, Note.D, Note.E, Note.F, Note.G, Note.A, Note.B]
         self.assertEqual(notes, expected)
     #endregion
