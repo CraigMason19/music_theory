bdb4358dc4aacfbbd569bd363cc5c8e4928cc962
SadHappy <craigjm19@gmail.com>
2025-04-07 19:53:39 +0100

    Added comments to diatonic scale

--- music_theory/scale_diatonic.py
+++ music_theory/scale_diatonic.py
@@ -13,7 +13,25 @@ from music_theory.scale_type import ScaleType
 
 
 class DiatonicScale(Scale):
+    """ An extended scale class that represents a diatonic scale (7 notes).
+
+    Attributes:
+        tonic:
+            A property that returns the tonic (root). 
+
+    Methods:        
+        __init__(self, root, scale_type):
+            Initialises the root and scale type.
+    """
     def __init__(self, root, scale_type=ScaleType.Major):
+        """ Builds the scale from a root note and a scale type. 
+
+        Args:
+            root:
+                The note to build the scale from.
+            scale_type:
+                The type of scale that contains 7 notes (i.e. not pentantonic or blues)      
+        """
         super(DiatonicScale, self).__init__(root, scale_type)
         
         if self.num_notes != 7:
@@ -68,7 +86,12 @@ class DiatonicScale(Scale):
 
     @property
     def subtonic(self):
-        # For Minor scales, the 7th note is the subtonic
+        """ For Minor scales ONLY, the 7th note is the subtonic rather than a 
+            leading tone.
+
+        Returns:
+            A Note.
+        """
         if self.type == ScaleType.Minor:
             return self.notes[6]
         
