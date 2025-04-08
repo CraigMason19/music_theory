de0442a06f6617a6590bde8ab61f204739b9ba7e
SadHappy <craigjm19@gmail.com>
2025-04-08 16:56:00 +0100

    added tests for subtonics and leading tones

--- music_theory_tests/test_scale_diatonic.py
+++ music_theory_tests/test_scale_diatonic.py
@@ -69,6 +69,26 @@ class TestDiatonicScale(unittest.TestCase):
         expected = Note.A
         self.assertEqual(ds.tonic, expected)
 
+    def test_diatonic_scale_major_doesnt_have_subtonic(self):
+        ds = DiatonicScale(Note.C, ScaleType.Major)
+
+        with self.assertRaises(AttributeError):
+                _ = ds.subtonic
+
+    def test_diatonic_scale_major_has_leading_tone(self):
+        ds = DiatonicScale(Note.C, ScaleType.Major)
+        self.assertEqual(ds.leading_tone, Note.B)
+
+    def test_diatonic_scale_minor_doesnt_have_leading_tone(self):
+        ds = DiatonicScale(Note.C, ScaleType.Minor)
+
+        with self.assertRaises(AttributeError):
+                _ = ds.leading_tone
+
+    def test_diatonic_scale_minor_has_subtonic(self):
+        ds = DiatonicScale(Note.C, ScaleType.Minor)
+        self.assertEqual(ds.subtonic, Note.Bb)
+
     #endregion
 
 
