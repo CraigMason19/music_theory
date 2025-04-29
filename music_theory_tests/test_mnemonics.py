5ab1d61fbc0d15240e6dd657670ede8073ac0e47
SadHappy <craigjm19@gmail.com>
2025-04-29 12:16:29 +0100

    renamed guitar tuning test, added empty list test

--- music_theory_tests/test_mnemonics.py
+++ music_theory_tests/test_mnemonics.py
@@ -15,17 +15,14 @@ import _setup
 from music_theory.mnemonics import Mnemonics
 
 class TestChords(unittest.TestCase):
-    #---------------------------------------------------------------------------
-    # Tests
-    #---------------------------------------------------------------------------
 
-    #region tunings
-
-    def test_custom_instrument_00(self):
+    def test_random_guitar_tuning_mnemonic(self):
         m = Mnemonics.random(Mnemonics.standard_guitar_tuning)
         self.assertIn(m, Mnemonics.standard_guitar_tuning)
 
-    #endregion
+    def test_random_raises_on_empty_list(self):
+        with self.assertRaises(IndexError):
+            Mnemonics.random([])
 
 if __name__ == '__main__': # pragma: no cover
     unittest.main()
