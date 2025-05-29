73f2514472cf5fbfca5da43833133d9d6dc47dc9
SadHappy <craigjm19@gmail.com>
2025-05-29 14:36:54 +0100

    Fixed minor typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     author="Craig Mason",
-    description="A toolkit forworking with and exploring music theory concepts.",
+    description="A toolkit for working with and exploring music theory concepts.",
     url="https://github.com/CraigMason19/music_theory",
     classifiers=[
         "Programming Language :: Python :: 3",
