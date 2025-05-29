fc4ad9564b99f34ed802cf8aee414484912c3898
SadHappy <craigjm19@gmail.com>
2025-05-29 14:13:43 +0100

    Added extra information to setup

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="music_theory",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    author="Craig Mason",
+    description="A toolkit forworking with and exploring music theory concepts.",
+    url="https://github.com/CraigMason19/music_theory",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
