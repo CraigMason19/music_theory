7e153247ef913c5a314b76852a694fd31d75437d
SadHappy <craigjm19@gmail.com>
2025-06-06 08:33:56 +0100

    Updaded package imports

--- music_theory/__init__.py
+++ music_theory/__init__.py
@@ -1,6 +1,12 @@
 from music_theory.chord_type import ChordType
 from music_theory.chords import Chord
+from music_theory.instrument import StringInstrument
+from music_theory.intervals import Interval
+from music_theory.key_type import KeyType
+from music_theory.keys import Key
+from music_theory.mnemonics import Mnemonics
 from music_theory.notes import Note
+from music_theory.progressions import Progression, NumeralProgressions, SongProgressions, NumeralCadences, chords_from_progression     
 from music_theory.scale_diatonic import DiatonicScale
 from music_theory.scale_type import ScaleType
 from music_theory.scales import Scale
