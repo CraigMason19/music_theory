0773b31a62f4769b5a0be6dd06dda9002b919a2c
SadHappy <craigjm19@gmail.com>
2025-06-06 08:33:36 +0100

    Added tests for KeyType

---
+++ music_theory/key_type.py
@@ -0,0 +1,98 @@
+#-------------------------------------------------------------------------------
+# Name:        key_type.py
+#
+# Notes:       Contains a class representing either a minor or major key.
+#
+#-------------------------------------------------------------------------------
+
+import random
+from enum import Enum
+
+class KeyType(Enum):
+    """ Represents a key type. Keys are either Major or Minor.
+    
+    Attributes:
+        Type attributes:
+            2 class attributes representing a Enum.
+
+    Methods:
+        items(cls):
+            A class method to return the enums as a list.
+        random(cls):
+            A class method to return a random key type.
+        parallel(self):
+            A property that returns a parallel (opposite) key type.
+        __str__(self):
+            Returns the name of the key type.
+        __repr__(self):
+            Returns the Enum name.
+    """
+    (Major, Minor) = range(2) 
+
+    @classmethod    
+    def items(cls):
+        """ A class method that returns a list of the key enumerations. 
+
+            e.g. [KeyType.Major, KeyType.Minor ... ]
+
+        Args:
+            None.
+
+        Returns:
+            A list of key types.
+        """  
+        return [n for n in cls]
+
+    @classmethod
+    def random(cls):
+        """ A class method that returns a random key type. 
+
+            e.g. KeyType.Major
+
+        Args:
+            None.
+
+        Returns:
+            A KeyType.
+        """  
+        return random.choice(cls.items())
+
+    @property
+    def parallel(self):
+        """ Returns the opposite KeyType. 
+
+            e.g. C Minor is the parallel of C Major.
+
+        Args:
+            None.
+
+        Returns:
+            A KeyType
+        """
+        return KeyType.Minor if (self.name == 'Major') else KeyType.Major
+
+    def __str__(self):
+        """ Returns a string representing the key type name. 
+
+            e.g. str(KeyType.Major) -> 'Major'
+
+        Args:
+            None.
+
+        Returns:
+            A string.
+        """
+        return self.name
+
+    def __repr__(self):
+        """ Returns a string representing the key types enum. 
+
+            e.g. repr(KeyType.Major) -> 'KeyType.Major'
+
+        Args:
+            None.
+
+        Returns:
+            A string representing the key type's name.
+        """
+        return f'KeyType.{self.name}'
---
+++ music_theory_tests/test_key_type.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        test_key_type.py
+#
+# Notes:       A test suite for the KeyType class.
+#  
+#-------------------------------------------------------------------------------
+
+import unittest
+
+import _setup
+
+from music_theory.keys import KeyType
+
+class TestKeyTypeAttributes(unittest.TestCase):
+    pass
+
+class TestKeyTypeMethods(unittest.TestCase):
+    def test_parallel_key_type_major_returns_minor(self):
+        parallel = KeyType.Major.parallel
+        expected = KeyType.Minor
+
+        self.assertEqual(parallel, expected)
+
+    def test_parallel_key_type_minor_returns_major(self):
+        parallel = KeyType.Minor.parallel
+        expected = KeyType.Major
+        
+        self.assertEqual(parallel, expected)
+
+class TestKeyTypeRepresentation(unittest.TestCase):
+    pass
+
+if __name__ == '__main__': # pragma: no cover
+    unittest.main()
