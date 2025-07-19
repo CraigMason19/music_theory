bdbd98c6a3dbc2b28a2775d77bb3239febbb766e
SadHappy <craigjm19@gmail.com>
2025-07-19 13:10:36 +0100

    split tests into multiple test classes

--- music_theory_tests/test_instrument.py
+++ music_theory_tests/test_instrument.py
@@ -1,13 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        test_instrument.py
-#
-# Notes:       A test suite for the instrument script.
-#
-# Links:        
-#
-# TODO:        
-#-------------------------------------------------------------------------------
-
 import unittest
 
 import _setup
@@ -17,36 +7,7 @@ from music_theory.intervals import Interval
 from music_theory.instrument import StringInstrument
 from music_theory.instrument_creator import create_standard_guitar, E_STANDARD_GUITAR, D_STANDARD_GUITAR, C_STANDARD_GUITAR
 
-class TestChords(unittest.TestCase):
-    #---------------------------------------------------------------------------
-    # setUpClass and tearDownClass run before and after all tests, called once
-    # NOTE - the camelCase syntax. Important that they are named this way.
-    #---------------------------------------------------------------------------
-
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
-    #---------------------------------------------------------------------------
-    # setUp and tearDown run before every single test.
-    #---------------------------------------------------------------------------
-
-    def setUp(self):
-        self.standard_guitar = create_standard_guitar(Note.E)
-
-    def tearDown(self):
-        pass
-
-    #---------------------------------------------------------------------------
-    # Tests
-    #---------------------------------------------------------------------------
-
-    #region Creation
-
+class TestInstrumentCreation(unittest.TestCase):
     def test_custom_instrument_00(self):
         instrument = StringInstrument([Note.B, Note.E, Note.A, Note.D, Note.G])
         expected = [Note.B, Note.E, Note.A, Note.D, Note.G]
@@ -66,78 +27,84 @@ class TestChords(unittest.TestCase):
         
         self.assertEqual(instrument.tuning, E_STANDARD_GUITAR.tuning)
 
-    #endregion
-
-    #region Capo
+class TestInstrumentCapo(unittest.TestCase):
     def test_add_a_capo_at_second_fret(self):
-        self.standard_guitar.add_capo(2)
+        guitar = create_standard_guitar()
+        guitar.add_capo(2)
 
         expected_tuning = [Note.Gb, Note.B, Note.E, Note.A, Note.Db, Note.Gb]
-        self.assertEqual(self.standard_guitar.tuning, expected_tuning)
+
+        self.assertEqual(guitar.tuning, expected_tuning)
 
     def test_add_a_capo_at_fourth_fret(self):
-        self.standard_guitar.add_capo(4)
+        guitar = create_standard_guitar()
+        guitar.add_capo(4)
 
         expected_tuning = [Note.Ab, Note.Db, Note.Gb, Note.B, Note.Eb, Note.Ab]
-        self.assertEqual(self.standard_guitar.tuning, expected_tuning)
+        self.assertEqual(guitar.tuning, expected_tuning)
 
     def test_add_a_capo_at_zero_fret(self):
-        self.assertRaises(ValueError, self.standard_guitar.add_capo, 0)
+        guitar = create_standard_guitar()
+
+        self.assertRaises(ValueError, guitar.add_capo, 0)
 
     def test_add_a_capo_at_negative_fret(self):
-        self.assertRaises(ValueError, self.standard_guitar.add_capo, -1)
+        guitar = create_standard_guitar()
 
-    #endregion
+        self.assertRaises(ValueError, guitar.add_capo, -1)
 
-    #region Adjust String
-
+class TestInstrumentAdjustString(unittest.TestCase):
     def test_adjust_string_up_00(self):
-        self.standard_guitar.adjust_string(0, Interval.M2, "u")
-        self.assertEqual(self.standard_guitar.tuning[0], Note.Gb)
+        guitar = create_standard_guitar()
+        guitar.adjust_string(0, Interval.M2, "u")
+
+        self.assertEqual(guitar.tuning[0], Note.Gb)
 
     def test_adjust_string_up_01(self):
-        self.standard_guitar.adjust_string(1, Interval.M2, "d")
-        self.assertEqual(self.standard_guitar.tuning[1], Note.G)
+        guitar = create_standard_guitar()
+        guitar.adjust_string(1, Interval.M2, "d")
+        
+        self.assertEqual(guitar.tuning[1], Note.G)
 
     def test_adjust_string_incorrect_string_index_00(self):
-        self.assertRaises(ValueError, self.standard_guitar.adjust_string, -1, Interval.M2, "d")
+        guitar = create_standard_guitar()
+
+        self.assertRaises(ValueError, guitar.adjust_string, -1, Interval.M2, "d")
 
     def test_adjust_string_incorrect_string_index_01(self):
-        self.assertRaises(ValueError, self.standard_guitar.adjust_string, 6, Interval.M2, "d")
+        guitar = create_standard_guitar()
 
-    #endregion
+        self.assertRaises(ValueError, guitar.adjust_string, 6, Interval.M2, "d")
 
-    #region Detune
-
+class TestInstrumentDetuning(unittest.TestCase):
     def test_detune_00(self):
-        self.standard_guitar.detune(Interval.M2)
-        self.assertEqual(self.standard_guitar.tuning, D_STANDARD_GUITAR.tuning)
+        guitar = create_standard_guitar()
+        guitar.detune(Interval.M2)
+ 
+        self.assertEqual(guitar.tuning, D_STANDARD_GUITAR.tuning)
 
     def test_detune_01(self):
         guitar = create_standard_guitar(Note.D)
         guitar.detune(Interval.M2)
         self.assertEqual(guitar.tuning, C_STANDARD_GUITAR.tuning)
 
-    #endregion
 
-
-    #region Note At Fret
-
+class TestInstrumentNoteAtFret(unittest.TestCase):
     def test_note_at_fret_incorrct_string_index_00(self):
-        self.assertRaises(ValueError, self.standard_guitar.note_at_fret, -1, 3)
+        self.assertRaises(ValueError, create_standard_guitar().note_at_fret, -1, 3)
 
     def test_note_at_fret_incorrct_string_index_01(self):
-        self.assertRaises(ValueError, self.standard_guitar.note_at_fret, 6, 3)
+        self.assertRaises(ValueError, create_standard_guitar().note_at_fret, 6, 3)
 
     def test_note_at_fret_incorrct_fret(self):
-        self.assertRaises(ValueError, self.standard_guitar.note_at_fret, 0, -1)
+        self.assertRaises(ValueError, create_standard_guitar().note_at_fret, 0, -1)
 
     def test_note_at_fret_zero_fret(self):
-        note = self.standard_guitar.note_at_fret(2, 0)
+        note = create_standard_guitar().note_at_fret(2, 0)
         self.assertEqual(Note.D, note)
 
     def test_note_at_fret_00(self):
-        note = self.standard_guitar.note_at_fret(2, 0)
+        note = create_standard_guitar().note_at_fret(2, 0)
         expected = Note.D
         self.assertEqual(note, expected)
 
@@ -147,93 +114,83 @@ class TestChords(unittest.TestCase):
         expected = Note.F
         self.assertEqual(note, expected)
 
-    #endregion
-
-    #region Notes in Chord
-
+class TestInstrumentNoteInChord(unittest.TestCase):
     def test_notes_in_chord_to_few_strings(self):
-        self.assertRaises(ValueError, self.standard_guitar.notes_in_chord, "x x 3 x 6")
+        self.assertRaises(ValueError, create_standard_guitar().notes_in_chord, "x x 3 x 6")
 
     def test_notes_in_chord_to_many_strings(self):
-        self.assertRaises(ValueError, self.standard_guitar.notes_in_chord, "x x x x x x 15")
+        self.assertRaises(ValueError, create_standard_guitar().notes_in_chord, "x x x x x x 15")
 
     def test_notes_in_chord_octave(self):
-        notes = self.standard_guitar.notes_in_chord("x 3 x 5 x x")
+        notes = create_standard_guitar().notes_in_chord("x 3 x 5 x x")
         expected = [Note.C, Note.C]
+
         self.assertEqual(notes, expected)
 
     def test_notes_in_chord_power_chord(self):
-        notes = self.standard_guitar.notes_in_chord("x 3 5 5 x x")
+        notes = create_standard_guitar().notes_in_chord("x 3 5 5 x x")
         expected = [Note.C, Note.G, Note.C]
+
         self.assertEqual(notes, expected)
 
     def test_notes_in_chord_open_d(self):
-        notes = self.standard_guitar.notes_in_chord("x x 0 2 3 2")
+        notes = create_standard_guitar().notes_in_chord("x x 0 2 3 2")
         expected = [Note.D, Note.A, Note.D, Note.Gb]
+
         self.assertEqual(notes, expected)
 
     def test_notes_in_chord_hendrix(self):
-        notes = self.standard_guitar.notes_in_chord("0 7 6 7 8 x")
+        notes = create_standard_guitar().notes_in_chord("0 7 6 7 8 x")
         expected = [Note.E, Note.E, Note.Ab, Note.D, Note.G]
+
         self.assertEqual(notes, expected)
 
-    #endregion
-
-    #region Intervals in Chord
-
+class TestInstrumentIntervalsInChord(unittest.TestCase):
     def test_intervals_in_power_chord(self):
-        intervals = self.standard_guitar.intervals_in_chord("x 3 5 5 x x")
+        intervals = create_standard_guitar().intervals_in_chord("x 3 5 5 x x")
         expected = [Interval.Unison, Interval.P5, Interval.Unison]
+
         self.assertEqual(intervals, expected)
 
     def test_intervals_in_empty_chord(self):
-        intervals = self.standard_guitar.intervals_in_chord("x x x x x x")
+        intervals = create_standard_guitar().intervals_in_chord("x x x x x x")
         self.assertEqual(intervals, [])
 
     def test_intervals_in_chord_to_few_strings(self):
-        self.assertRaises(ValueError, self.standard_guitar.intervals_in_chord, "x x 3 x 6")
+        self.assertRaises(ValueError, create_standard_guitar().intervals_in_chord, "x x 3 x 6")
 
     def test_intervals_in_chord_to_many_strings(self):
-        self.assertRaises(ValueError, self.standard_guitar.intervals_in_chord, "x x x x x x 15")
+        self.assertRaises(ValueError, create_standard_guitar().intervals_in_chord, "x x x x x x 15")
 
     def test_intervals_in_chord_octave(self):
-        intervals = self.standard_guitar.intervals_in_chord("x 3 x 5 x x")
+        intervals = create_standard_guitar().intervals_in_chord("x 3 x 5 x x")
         expected = [Interval.Unison, Interval.Unison]
+
         self.assertEqual(intervals, expected)
 
     def test_intervals_in_chord_power_chord(self):
-        intervals = self.standard_guitar.intervals_in_chord("x 3 5 5 x x")
+        intervals = create_standard_guitar().intervals_in_chord("x 3 5 5 x x")
         expected = [Interval.Unison, Interval.P5, Interval.Unison]
+
         self.assertEqual(intervals, expected)
 
     def test_intervals_in_chord_open_d(self):
-        intervals = self.standard_guitar.intervals_in_chord("x x 0 2 3 2")
+        intervals = create_standard_guitar().intervals_in_chord("x x 0 2 3 2")
         expected = [Interval.Unison, Interval.P5, Interval.Unison, Interval.M3]
+
         self.assertEqual(intervals, expected)
 
-    #region
+class TestInstrumentNotesInRiff(unittest.TestCase):
+    pass
 
-    #region Notes in riff
-
-
-
-    #endregion
-
-    #region __str__
-
+class TestInstrumentStringRepresentation(unittest.TestCase):
     def test_str_method(self):
         expected = "E, A, D, G, B, E"
-        self.assertEqual(str(self.standard_guitar), expected)
-
-    #endregion
-
-    #region __repr__
+        self.assertEqual(str(create_standard_guitar()), expected)
 
     def test_repr_method(self):
         expected = "StringInstrument(6, [E, A, D, G, B, E])"
-        self.assertEqual(repr(self.standard_guitar), expected)
-
-    #endregion
+        self.assertEqual(repr(create_standard_guitar()), expected)
 
 if __name__ == '__main__': # pragma: no cover
     unittest.main()
