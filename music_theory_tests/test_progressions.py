1555a334c74422ad2887a97586f01f7a866dd9f9
SadHappy <craigjm19@gmail.com>
2025-07-24 15:54:20 +0100

    Added tests for diminished, chords_from_progression returns a list of chords not strings

--- music_theory_tests/test_progressions.py
+++ music_theory_tests/test_progressions.py
@@ -5,29 +5,30 @@ import _setup
 from music_theory.progressions import chords_from_progression
 from music_theory.keys import Key, KeyType
 from music_theory.notes import Note
+from music_theory.chords import Chord, ChordType
 
 class TestChordsFromProgression(unittest.TestCase):
     def test_chord_progressions_C_major(self):
         prog = ['I', 'IV', 'V']
         
         result = chords_from_progression(Key(Note.C), prog)
-        expected = ['CM', 'FM', 'GM']
+        expected = [Chord(Note.C, ChordType.Major), Chord(Note.F, ChordType.Major), Chord(Note.G, ChordType.Major)]
         
         self.assertListEqual(result, expected)
 
-    def test_chord_progressions_A_major_upper_lower_numerals(self):
-        prog = ['i', 'I', 'iv', 'IV', 'vii', 'VII']
+    def test_chord_progressions_A_major_upper_lower_numerals_4th(self):
+        prog = ['iv', 'IV']
         
         result = chords_from_progression(Key(Note.A), prog)
-        expected = ['Am', 'AM', 'Dm', 'DM', 'Ab°', 'GM']
-        
+        expected = [Chord(Note.D, ChordType.Minor), Chord(Note.D, ChordType.Major)]
+
         self.assertListEqual(result, expected)
 
-    def test_chord_progressions_A_minor_upper_lower_numerals(self):
-        prog = ['i', 'I', 'iv', 'IV', 'vii', 'VII']
+    def test_chord_progressions_A_minor_upper_lower_numerals_4th(self):
+        prog = ['iv', 'IV']
         
         result = chords_from_progression(Key(Note.A, KeyType.Minor), prog)
-        expected = ['Am', 'AM', 'Dm', 'DM', 'Ab°', 'GM']
+        expected = [Chord(Note.D, ChordType.Minor), Chord(Note.D, ChordType.Major)]
         
         self.assertListEqual(result, expected)
 
@@ -46,6 +47,23 @@ class TestChordsFromProgression(unittest.TestCase):
 
         result = chords_from_progression(Key(Note.A), prog, error)
         expected = [error] * len(prog)
+
+        self.assertListEqual(result, expected)
+        
+class TestChordsFromProgressionDiminished(unittest.TestCase):
+    def test_C_Major_chords_diminished_7th(self):
+        prog = ['I', 'vii°']
+
+        result = chords_from_progression(Key(Note.C), prog)
+        expected = [Chord(Note.C, ChordType.Major), Chord(Note.B, ChordType.Diminished)]
+
+        self.assertListEqual(result, expected)
+
+    def test_C_Major_parallel_chords_diminished_2nd(self):
+        prog = ['I', 'ii°']
+
+        result = chords_from_progression(Key(Note.C), prog)
+        expected = [Chord(Note.C, ChordType.Major), Chord(Note.D, ChordType.Diminished)]
 
         self.assertListEqual(result, expected)
 
