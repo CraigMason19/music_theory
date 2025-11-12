c39b43433e2c0c379196771188d69357ea6d7b6b
SadHappy <craigjm19@gmail.com>
2025-11-12 17:54:16 +0000

    Upgraded minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="music_theory",
-    version="1.7.0",
+    version="1.8.0",
     packages=find_packages(),
     author="Craig Mason",
     description="A toolkit for working with and exploring music theory concepts.",
